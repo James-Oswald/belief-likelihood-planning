11192fc3e99f5107ecbaea271bc62bdab42bc74a
James Oswald <jamesoswald111@gmail.com>
2024-02-04 05:20:10 -0500

    Classical Planning Applicability

--- src/Classical.lean
+++ src/Classical.lean
@@ -1,22 +1,33 @@
 
 import src.Finset
 
-structure Action (Facts : Finset String) where
-  Name: String
-  Pre : Finsubset Facts
-  Add : Finsubset Facts
-  Del : Finsubset Facts
+structure Action (F : Finset String) where
+  Name : String
+  Pre : Finsubset F
+  Add : Finsubset F
+  Del : Finsubset F
+deriving BEq
 
 /- A Classical Planning Problem -/
-structure CPP where
-  Facts : Finset String
-  Actions : Finset (Action Facts)
-  --Action names are unique
-  actions_nodup : List.Nodup ActionNames
-  Init : Finsubset Facts
-  Goals : Finset (Finsubset Facts)
+structure PlanProblem where
+  F : Finset String         -- the set of facts
+  A : Finset (Action F)     -- the set of actions
+  S₀ : Finsubset F          -- the initial state
+  G : Finset (Finsubset F)
 
-def CPP.ActionNames (π : CPP) : Finset String :=
-  π.Actions.map Action.Name
+-- A plan for a planning problem π is a list of actions
+-- that are all in π.A
+structure Plan (π : PlanProblem) where
+  l : List (Action π.F)
+  h : ∀ a ∈ l, a ∈ π.A
 
---def Plan (π : CPP) : Type := List Action (Facts π)
+--Plan applicability defines the semantics of what it means to be a plan.
+def PlanApplicable (π : PlanProblem) (p : Plan π) : Prop :=
+  let rec loop (π : PlanProblem) (p : Plan π) (s : Finsubset π.F) : Prop :=
+    match p with
+    | ⟨[], _⟩  => s ∈ π.G
+    | ⟨a::l, h⟩ =>
+      let s' := (s \ a.Del) ∪ a.Add; -- New state
+      let p' := ⟨l, λa' h'=> by simp [h a' (List.mem_cons_of_mem _ h')]⟩; -- Rest of plan
+      if a.Pre ⊆ s then loop π p' s' else False
+  loop π p π.S₀
--- src/Finset.lean
+++ src/Finset.lean
@@ -1,80 +1,22 @@
-
 /-
-So while writing this, I realized that to define finsets you
-need Quotient types, which is why Mathlib defines finsets as
-MultiSets.
+This file contains the definition of a Finsubset
+as well as some custom machinery and theorems for Finsets
 -/
 
-import Std.Data.List
-
-import src.Util
+import Mathlib.Data.Finset.Basic
 
 /-
-Map a function over a list, but do not include duplicates
+It is not obvious at all but Finset.Sort is what
+allows you to actually convert a Finset to a List
+in a computable way, Finset.toList is a psyop.
+Also has a Repr instance.
 -/
-def List.mapNodup [BEq β] (f : α -> β) (l : List α) : List β :=
-match l with
-| [] => []
-| h::t =>
-  let ft : List β := List.mapNodup f t;
-  let fh : β := f h;
-  if List.elem fh ft then ft else fh::ft
+import Mathlib.Data.Finset.Sort
 
-/- List.mapNodup creates a list with no duplicates -/
-theorem List.mapNodup_Nodup [BEq β] [LawfulBEq β] (f : α -> β) (l : List α) :
-List.Nodup (List.mapNodup f l) := by
-  induction l with
-  | nil => simp [Nodup, List.mapNodup]
-  | cons h t ih =>
-    simp [Nodup, List.mapNodup]
-    cases H : List.elem (f h) (mapNodup f t)
-    case true =>
-      simp
-      exact ih
-    case false =>
-      simp
-      apply And.intro
-      case left =>
-        intros a' H2 H3
-        rw [←H3] at H2
-        have H4 := elem_eq_true_of_mem H2
-        rw [H4] at H
-        simp at H
-      case right =>
-        exact ih
 
-/-
-A Finset is a set with a finite number of elements
-It is represented as a list with a no duplicates constraint
--/
-structure Finset (α : Type) where
-  members : List α
-  unique : members.Nodup
-
-instance : Coe (Finset α) (List α) := ⟨Finset.members⟩
-instance {H : x.Nodup} : CoeDep (List α) x (Finset α) := ⟨x, H⟩
-instance : HasSubset (Finset α) := ⟨(·.members ⊆ ·.members)⟩
-instance : Membership α (Finset α) := ⟨(· ∈ ·.members)⟩
-
-def Finset.equals (f1 f2 : Finset α) : Prop := f1 ⊆ f2 ∧ f2 ⊆ f1
-
-instance Finset.decEq_finset [DecidableEq α] (f1 f2 : Finset α) :
-Decidable (Finset.equals f1 f2) := by
-  have H : @DecidableRel (List α) (· ⊆ ·) := inferInstance
-  have H1 : Decidable (f1 ⊆ f2) := H f1 f2
-  have H2 : Decidable (f2 ⊆ f1) := H f2 f1
-  have H3 : Decidable (f1 ⊆ f2 ∧ f2 ⊆ f1) := inferInstance
-  assumption
-
-instance [DecidableEq α] : BEq (Finset α) := ⟨λf1 f2 => decide (Finset.equals f1 f2)⟩
-
-def Finset.map {α β : Type} [BEq β] [LawfulBEq β] (f : α -> β) (s : Finset α) : Finset β :=
-  ⟨List.mapNodup f s, List.mapNodup_Nodup f s⟩
-
-@[simp]
-theorem Finset.self_subset (f : Finset α) : f ⊆ f := by
-  intros _ H
-  exact H
+theorem Finset.self_subset {F : Finset α} : F ⊆ F := by
+  intro x
+  exact id
 
 /-
 A finite subset of a Finset F is a Finset with
@@ -84,15 +26,36 @@ structure Finsubset (F : Finset α) where
   subset : Finset α
   h : subset ⊆ F
 
---Dependent Coercion of a Finset x to a Finsubset
-instance : CoeDep (Finset α) x (Finsubset x) := ⟨x, Finset.self_subset x⟩
-instance : HasSubset (Finsubset α) := ⟨(·.subset ⊆ ·.subset)⟩
+instance : CoeDep (Finset α) x (Finsubset x) := ⟨x, Finset.self_subset⟩
 
-def Finsubset.Mem {F : Finset α} (s : Finsubset F) (a : α) : Prop :=
-  a ∈ s.subset
+--BEq Instances
+instance Finsubset.Beq [DecidableEq α] {x : Finset α} : BEq (Finsubset x) :=
+  ⟨(·.subset == ·.subset)⟩
+instance {F : Finset String} : BEq (Finsubset F) := by exact Finsubset.Beq
 
+--Subset Instances
+def Finsubset.Subset {F : Finset α} (S1 S2: Finsubset F) : Prop := S1.subset ⊆ S2.subset
+
+instance : HasSubset (Finsubset α) := ⟨Finsubset.Subset⟩
+
+instance [DecidableEq α] {F : Finset α} {a b : Finsubset F} :
+Decidable (a ⊆ b) := by
+  simp [Subset, Finsubset.Subset]
+  infer_instance
+
+--Other Set Opperation Instances
 instance {F : Finset α} : Membership α (Finsubset F) := ⟨(· ∈ ·.subset)⟩
 
+instance [DecidableEq α] {F : Finset α} : SDiff (Finsubset F) where
+  sdiff a b := ⟨a.subset \ b.subset, by
+    intros x H
+    exact a.h (Finset.sdiff_subset _ _ H)
+  ⟩
+
+instance [DecidableEq α] {F : Finset α} : Union (Finsubset F) where
+  union a b := ⟨a.subset ∪ b.subset, Finset.union_subset a.h b.h⟩
+
+@[simp]
 theorem Finsubset.Mem_in_superset {F : Finset α} (S : Finsubset F) (x : α) :
 x ∈ S → x ∈ F := by
   intro H
